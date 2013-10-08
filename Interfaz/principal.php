a75623edebd2d24d1507a13fef3f0c81492d8493
JoelValdivia <jooelvaldivi@gmail.com>
2013-10-08 14:06:51 -0500

    Intefaz

---
+++ Interfaz/principal.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>EED</title>
+	 <link rel="stylesheet" href="estiloEED.css"/>
+
+</head>
+<body>
+ <form action="inicio.php" method="post">
+     <a href="http://www.udg.mx" title="Ir a Universidad De Guadalajara"><img src="logo.jpg" alt="GIF" id="logoUDG"></a>
+        <label id="lblusu">USUARIO</label>
+
+        <input title="USUARIO" type="text" name="usuario" id="usu" size="10" maxlength="100" value=""/>
+
+        <label id="lblcontra">CONTRASE&Ntilde;A</label>
+
+        <input title="CONTRASE&Ntilde;A" type="password" name="contra" id="contra" size="10" maxlength="100"/>
+
+        <input type="submit" id="btningresar" title="Pulsa para ingresar" value="INGRESAR">
+<a href="http://www.cutonala.udg.mx" title="Ir a CUTonal&aacute;">
+  <img src="dummylogo.png" alt="GIF" id="logocutonala">
+</a>
+
+        </form>
+	
+</body>
+</html>
